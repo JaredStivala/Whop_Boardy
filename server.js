488b7a8e30cb6a23bcb49e212a0a410035cc8a1e
JaredStivala <jstivala1@babson.edu>
2025-06-02 00:22:12 -0400

    remove /app route

--- server.js
+++ server.js
@@ -502,10 +502,6 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.get('/app', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'app.html'));
-});
-
 app.get('/directory', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'directory.html'));
 });
